b7cc4b39bf30b9bcd7d7300dc45e55f11e9b5b9e
doudgaya <arbdoule@yahoo.com>
2024-05-11 22:59:20 +0100

    loading

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logoicon from '@/public/img/logo-icon.png'
+import Image from 'next/image'
+
+const loading = () => {
+  return (
+    <div className=' h-screen w-full flex justify-center items-center'>
+        <div className="">
+            <Image src={logoicon} className=' h-10 w-10 animate-pulse' alt='' />    
+        </div>
+    </div>
+  )
+}
+
+export default loading
