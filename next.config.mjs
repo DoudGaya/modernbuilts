f109ceaec467148f2d4fa8cf4f98e4d6667f067f
Abdulrahman Dauda Gaya <57319135+DoudGaya@users.noreply.github.com>
2026-05-18 18:27:24 +0100

    fix(next): suppress uploadthing webpack warnings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      { module: /node_modules\/@uploadthing/ }
+    ];
+    return config;
+  }
+};
 
 export default nextConfig;
